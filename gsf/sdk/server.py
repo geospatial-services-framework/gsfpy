6acbb7a50bd020f91319e0abc4f305f7b1314d18
hgs-mjacquin <mjacquin@harris.com>
2023-05-30 17:09:11 +0200

    Added a request to server-info to get description and version of the server
    Added a getJobs function that returns a job list with jobStatus filtering
    And added a path parameter to the _http_get function

--- gsf/sdk/server.py
+++ gsf/sdk/server.py
@@ -18,7 +18,10 @@ class Server(BaseServer):
         self._services_path = 'services'
         self._url = urlunparse(('http', ':'.join((self._server, self._port)),
                                 '', None, None, None))
-        self._services_info = dict()
+
+        self._info_path = 'reports/server-info'
+        self._server_info = self._http_get(self._info_path)
+        self._services_list = []
 
     @property
     def name(self):
@@ -27,14 +30,38 @@ class Server(BaseServer):
     @property
     def port(self):
         return self._port
+    
+    @property
+    def description(self):
+        """Returns the description of the server"""
+        return self._server_info['description']
 
+    @property
+    def version(self):
+        """Returns the GSF version of the server"""
+        return self._server_info['version']
+    
+    @property
+    def requestHandlers(self) :
+        """Returns a list of requestHandlers"""
+        handlersList = []
+        for handler in self._server_info['configuration']['requestHandlers']:
+            handlersList.append(handler['type'])
+        return handlersList
+
+    @property
     def services(self):
         """Returns a list of services"""
-        info = self._http_get()
-        service_list = []
-        for service in info['services']:
-            service_list.append(service['name'])
-        return service_list
+        services_info = self._http_get(self._services_path)
+        self._services_list = []
+        for service in services_info['services']:
+            self._services_list.append(service['name'])
+        return self._services_list
+    
+    @property
+    def info(self):
+        """ Returns server information full"""
+        return self._server_info
 
     def service(self, service_name):
         """
@@ -43,23 +70,49 @@ class Server(BaseServer):
         :param service_name: The name of the service to return.
         :return: Returns the gsf.service object.
         """
+        if not service_name in self._services_list : 
+            return None
         return Service('/'.join((self._url, self._services_path, service_name)))
 
     def job(self, job_id):
         """
-
         :param jobID:
-        :return:
+        :return: a Job object
         """
         return Job('/'.join((self._url, 'jobs', str(job_id))))
+<<<<<<< Updated upstream
 
     @lru_cache(maxsize=None)
     def _http_get(self):
+=======
+    
+    def getJobs(self, jobStatus=None):
+        """
+        :param jobStatus: Filters output with jobStatus 
+        :return: a job list
+        """
+        jobs = self._http_get('jobs')
+        jobs_list = []
+        for job in jobs['jobs'] :
+            if jobStatus is None or job['jobStatus'] == jobStatus :
+                jobs_list.append(job)
+        return jobs_list
+    
+    @property
+    def jobs(self):
+        """
+        :return: all jobs as a list
+        """
+        return self.getJobs()
+    
+    @lru_cache(maxsize=None)
+    def _http_get(self, path=None):
+>>>>>>> Stashed changes
         """
         :return:
         """
         try:
-            response = requests.get('/'.join((self._url, self._services_path)))
+            response = requests.get('/'.join((self._url, path)))
             if response.status_code >= 400:
                 raise ServerNotFoundError(f'HTTP code {response.status_code}, Reason: {response.text}')
         except ConnectionError as err:
